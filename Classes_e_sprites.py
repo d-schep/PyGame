c67e823441b6b8448c3282910ea0ad0776a00298
d-schep <schep.danny@gmail.com>
2025-05-12 14:59:24 -0300

    pré sprites

--- Classes_e_sprites.py
+++ Classes_e_sprites.py
@@ -10,7 +10,7 @@ import time
 class Jogador(pygame.sprite.Sprite):
     def __init__(self,group,assets):
         pygame.sprite.Sprite.__init__(self)
-        self.image = assets[PERSONAGEM_IMG]
+        self.image = assets[PERSONAGEM]
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.centerx = LARGURA/2
